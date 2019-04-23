b961b2b2c2eac46d56611834f584d694949a3fa2
Robert Grant <rcarver515@gmail.com>
2019-04-23 13:01:27 -0500

    Initial commit w/ folders & files and public & routing code

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Elizabeth",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        4,
+        3,
+        1,
+        4,
+        2,
+        1,
+        4,
+        5,
+        3,
+        5
+      ]
+  },
+]
+module.exports = friends;
